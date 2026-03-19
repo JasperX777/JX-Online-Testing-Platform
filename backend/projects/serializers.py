5e60ca04d6da44a42b81d4370662ec437fe88d01
Jasper <jasperxue732@gmail.com>
2026-03-20 09:16:31 +1300

    update serializer for project API

--- backend/projects/serializers.py
+++ backend/projects/serializers.py
@@ -2,9 +2,9 @@ from rest_framework import serializers
 from .models import Project
 
 class ProjectSerializer(serializers.ModelSerializer):
-    owner_username = serializers.ReadOnlyField(source='owner.username', read_only=True)
+    owner_username = serializers.CharField(source='owner.username', read_only=True)
 
     class Meta:
         model = Project
-        fields = 'id', 'name', 'description', 'owner_username', 'created_at'
+        fields = ('id', 'name', 'description', 'owner', 'owner_username', 'created_at')
         read_only_fields = ('owner', 'owner_username', 'created_at')
