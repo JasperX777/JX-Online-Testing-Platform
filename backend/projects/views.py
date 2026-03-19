a6b8cc27df190324d801bba5f8e10260e23a9f07
Jasper <jasperxue732@gmail.com>
2026-03-20 09:14:29 +1300

    enforce role-based project read/write permissions

--- backend/projects/views.py
+++ backend/projects/views.py
@@ -1,24 +1,28 @@
-from rest_framework import viewsets
+from django.db.models import Q
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ModelViewSet
 
 from .models import Project
+from .permissions import IsProjectOwnerOrAdminForWrite
 from .serializers import ProjectSerializer
 
-class ProjectViewSet(viewsets.ModelViewSet):
+
+class ProjectViewSet(ModelViewSet):
     serializer_class = ProjectSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsProjectOwnerOrAdminForWrite]
 
     def get_queryset(self):
         user = self.request.user
-
-        if user.is_superuser:
-            return Project.objects.all()
-
         role = getattr(getattr(user, 'profile', None), 'role', None)
-        if role == 'admin':
+
+        if user.is_superuser or role == 'admin':
             return Project.objects.all()
 
-        return Project.objects.filter(owner=user)
+        if role == 'developer':
+            return Project.objects.filter(Q(owner=user) | Q(project_members__user=user)).distinct()
+
+        # tester: can only see assigned projects
+        return Project.objects.filter(project_members__user=user).distinct()
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
